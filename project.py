6fd1023a840f8d49ac339c4cd750991f91b7f6d6
Rogier <r.monshouwer@student.utwente.nl>
2017-10-16 19:18:47 +0200

    confusion matrix

--- project.py
+++ project.py
@@ -1,9 +1,8 @@
-
 import nltk
 import csv
 from nltk.util import ngrams
 import textblob.classifiers as classifiers
-
+import math
 
 train_data = []
 with open('train.csv', 'r') as csvfile:
@@ -35,10 +34,11 @@ with open('test_with_solutions.csv', 'r') as csvfile:
     for row in insult_reader:
         test_data.append((row['Comment'], row['Insult']))
 
-
 # test_sets = [(nltk.tokenize(n), insult) for (n, insult) in train_data]
 
 test_results = {'tp': 0, 'fp': 0, 'tn': 0, 'fn': 0}
+
+
 for (comment, insult) in test_data:
     label = cl.classify(comment)
     if insult == '1':
@@ -52,10 +52,24 @@ for (comment, insult) in test_data:
         elif label == '0':
             test_results['tn'] = test_results.get('tn', 0) + 1
 
-def print_confusion_matrix(results, positive_label):
+def print_confusion_matrix(results, positive_label, classifier_name):
+    print("Confusion matrix for {}".format(classifier_name))
     print("Positive: {}".format(positive_label))
-    print("          | Positive        | Negative")
-    print("Positive  | {:5}           | {:5}".format(results['tp'], results['fp']))
-    print("Negative  | {:5}           | {:5}".format(results['fn'], results['tn']))
+    print("                    | Actual          | -")
+    print("                    | Positive        | Negative")
+    print("Predicted Positive  | {:5}           | {:5}".format(results['tp'], results['fp']))
+    print("-         Negative  | {:5}           | {:5}".format(results['fn'], results['tn']))
+    precision = results['tp'] * 100 / (results['tp'] + results['fp'])
+    recall = results['tp'] * 100 / (results['tp'] + results['fn'])
+    matthews1 = results['tp'] * results['tn'] - results['fp'] * results['fn']
+    matthews2 = (results['tp'] + results['fp']) * (results['tp'] + results['fn']) * (results['tn'] + results['fp']) *\
+                (results['tn'] + results['fn'])
+    matthews = float(matthews1) * 100 / math.sqrt(matthews2)
+    print("Precision    : {:05.2f}%".format(precision))
+    print("Recall       : {:05.2f}%".format(recall))
+    print("Matthews     : {:05.2f}".format(matthews))
 
-print_confusion_matrix(test_results, 'Insult')
+    print("-------------------------------------------------")
+
+
+print_confusion_matrix(test_results, 'Insult', 'NaiveBayes')
